7ab8cf8b4acf818ecafb34aef7151f08e51a2889
yanasavva <yana1579@yandex.ru>
2018-04-01 14:28:41 +0300

    task 4 edited

--- yanasavva/week3/lines.cpp
+++ yanasavva/week3/lines.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 int main() {
-    bool arr[256];
-    for (int k = 0; k < 256; ++k) {
-        arr[k] = false;
-    }
+
+    unordered_map<char, bool> inclusions;
 
     string line1, line2;
     cin >> line1 >> line2;
-    for (unsigned char  i : line1) {
-        arr[i] = true;
+    for ( char  i : line1) {
+        inclusions[i] = true;
     }
-    for (unsigned  char  i : line2) {
-        arr[i] = false;
+    for ( char  i : line2) {
+        inclusions[i] = false;
     }
-    for (int j = 0; j < 256; ++j) {
-        if (arr[j])
-            cout << (unsigned char)j;
+    for ( char  i : line1) {
+        if (inclusions[i])
+            cout << i;
     }
     cout << endl;
     return 0;
